81dabec4f572f54a596c7337694124e2c9eabb83
KashF <Kashfarhadi@gmail.com>
2019-07-29 20:10:14 -0400

    added a template

--- app_dir/__init__.py
+++ app_dir/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 flask_app = Flask(__name__)
 
--- app_dir/routes.py
+++ app_dir/routes.py
@@ -1,11 +1,13 @@
+from flask import render_template
 from app_dir import flask_app
 
 @flask_app.route("/")
 @flask_app.route("/index")
 def home():
-    return "<h1>Hello, World</h1>"
+    return render_template('base.html')
+
 
 
 @flask_app.route("/about")
 def about():
-    return "<h1>About Page<h1>"
+    return ""
