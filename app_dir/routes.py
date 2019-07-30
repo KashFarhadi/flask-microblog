aafd73096e80b7224dc3dbde7377a05e4ea08244
KashF <Kashfarhadi@gmail.com>
2019-07-29 20:25:57 -0400

    setup flake8 and started html template

--- app_dir/routes.py
+++ app_dir/routes.py
@@ -1,13 +1,25 @@
 from flask import render_template
 from app_dir import flask_app
 
+
 @flask_app.route("/")
 @flask_app.route("/index")
 def home():
-    return render_template('base.html')
 
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('base.html', posts=posts)
 
 
 @flask_app.route("/about")
 def about():
-    return ""
+    return "<h1>About Page </h1>"
