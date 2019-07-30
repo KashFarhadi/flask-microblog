8efe599279f9c48c4f43598bf615b37b28f2deb0
KashF <Kashfarhadi@gmail.com>
2019-07-29 23:21:30 -0400

    started part 4 flask migrate and sqlalchemy. Gotta sleep

--- microblog.py
+++ microblog.py
@@ -1,4 +1,11 @@
 from app_dir import flask_app
+from app_dir.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
 
 if __name__ == '__main__':
     flask_app.run(debug=True)
